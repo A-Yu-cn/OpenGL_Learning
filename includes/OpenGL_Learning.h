4afdf8f36d79b38842af05e420ffdf6078d76706
aYu <2426774691@qq.com>
2022-03-15 22:22:59 +0800

    initialize the project

---
+++ includes/OpenGL_Learning.h
@@ -0,0 +1,18 @@
+﻿// OpenGL_Learning.h: 标准系统包含文件的包含文件
+// 或项目特定的包含文件。
+
+#include <filesystem>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include "stb_image.h"
+
+#include "shader_s.h"
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <iostream>
+
+// TODO: 在此处引用程序需要的其他标头。
